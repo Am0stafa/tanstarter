c5901f91b09121caf5ee5ab14ef186b3d78e7d4e
Raphaël Balet <raphael.balet@outlook.com>
2026-05-31 17:24:52 +0200

    build(vite.config.ts): define global __APP_VERSION__ variable (#79)
    Create a global `__APP_VERSION__` and make it available.
    
    ### Note
    As this variable is often use, this could be a neat addition to the
    library, to also show howe to declare such variable.
    
    Feel free to close the PR if you don't see the use of such addition ;)_

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
+import { version } from "./package.json";
 
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
   resolve: {
     tsconfigPaths: true,
   },
